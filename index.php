107ebf25118ada855f69fb44afe46aa3e8ac1a98
Nielsington <declercqniels@hotmail.com>
2023-03-23 18:33:24 +0100

    Add route
    - Add database connection

--- index.php
+++ index.php
@@ -6,11 +6,14 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-//include all your model files here
+require 'config.php';
+require 'Model/DatabaseManager.php';
 require 'Model/Article.php';
-//include all your controllers here
 require 'Controller/HomepageController.php';
 require 'Controller/ArticleController.php';
+
+$databaseManager = new databaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
+$databaseManager->connect();
 
 // Get the current page to load
 // If nothing is specified, it will remain empty (home should be loaded)
@@ -23,12 +26,12 @@ switch ($page) {
         // This is shorthand for:
         // $articleController = new ArticleController;
         // $articleController->index();
-        (new ArticleController())->index();
+        (new ArticleController($databaseManager))->index();
         break;
     case 'articles-show':
-        // TODO: detail page
+        (new ArticleController($databaseManager))->show();
     case 'home':
     default:
-        (new HomepageController())->index();
+        (new HomepageController($databaseManager))->index();
         break;
 }
