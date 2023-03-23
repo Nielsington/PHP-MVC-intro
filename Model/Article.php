befd986bec53db2feee2959d1af8f0a3ea948c41
Nielsington <declercqniels@hotmail.com>
2023-03-23 18:34:16 +0100

    Add date format function

--- Model/Article.php
+++ Model/Article.php
@@ -7,16 +7,20 @@ class Article
     public string $title;
     public ?string $description;
     public ?string $publishDate;
+    public int $id;
 
-    public function __construct(string $title, ?string $description, ?string $publishDate)
+    public function __construct(string $title, ?string $description, ?string $publishDate, int $id)
     {
         $this->title = $title;
         $this->description = $description;
         $this->publishDate = $publishDate;
+        $this->id = $id;
     }
 
-    public function formatPublishDate($format = 'DD-MM-YYYY')
+    public function formatPublishDate($format = 'd-m-Y  H:i:s A')
     {
-        // TODO: return the date in the required format
+        $formatDate = date($format, strtotime($this->publishDate));
+        return $formatDate;
+
     }
 }
