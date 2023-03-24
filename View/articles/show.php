1495d6675f3a31be4a76f3159e42b93ebb65ef34
Nielsington <declercqniels@hotmail.com>
2023-03-24 16:17:59 +0100

    Add conditions for previous/next article links
    - Adjust previous/next article href

--- View/articles/show.php
+++ View/articles/show.php
@@ -1,14 +1,31 @@
 <?php require 'View/includes/header.php'?>
 
-<?php // Use any data loaded in the controller here ?>
+<?php 
+    global $databaseManager; 
+    $url = (new ArticleController($databaseManager))->getUrl();
+    $lastPage = count($ids)-1;
+
+    if ($currentId === 0) {
+        $previousPage = $ids[0];
+        $nextPage = $ids[$currentId+1];
+    } 
+    elseif ($currentId === $lastPage) {
+        $previousPage = $ids[$currentId-1];
+        $nextPage = $ids[$lastPage];
+    } 
+    else {
+        $previousPage = $ids[$currentId-1];
+        $nextPage = $ids[$currentId+1];
+    }
+?>
 
 <section>
     <h1><?= $article->title ?></h1>
     <p><?= $article->formatPublishDate() ?></p>
     <p><?= $article->description ?></p>
 
-    <a href="?page=articles-show&id=<?= $ids[$currentId-1] ?>">Previous article</a>
-    <a href="?page=articles-show&id=<?= $ids[$currentId+1] ?>">Next article</a>
+    <a href="<?= $url?> <?= $previousPage ?>">Previous article</a>
+    <a href="<?= $url?> <?= $nextPage ?>">Next article</a>
 </section>
 
 <?php require 'View/includes/footer.php'?>
